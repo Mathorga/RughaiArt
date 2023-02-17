3933d945932c1759cf3685f6347d7f86a3b48f9a
Luka Micheletti <l.micheletti@ebw.it>
2023-02-18 00:39:02 +0100

    added tiled files in tiled folder in tiled mammt

---
+++ rughai/tiles/tiled/pit_tileset.tsx
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="pit_depth" tilewidth="8" tileheight="16" tilecount="14" columns="7">
+ <image source="../RughaiArt/rughai/tiles/pit_tileset.png" width="56" height="32"/>
+ <tile id="3" probability="80"/>
+ <tile id="4" probability="10"/>
+ <tile id="11" probability="10"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="" color="#ff7700" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00e9ff" tile="-1" probability="1"/>
+   <wangcolor name="" color="#ff00d8" tile="-1" probability="1"/>
+   <wangcolor name="" color="#ffff00" tile="-1" probability="1"/>
+   <wangcolor name="" color="#a000ff" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ffa1" tile="-1" probability="1"/>
+   <wangcolor name="" color="#ffa8a8" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="2,0,3,0,0,0,0,0"/>
+   <wangtile tileid="1" wangid="4,0,4,0,2,0,3,0"/>
+   <wangtile tileid="2" wangid="4,0,3,0,0,0,3,0"/>
+   <wangtile tileid="3" wangid="4,0,4,0,4,0,4,0"/>
+   <wangtile tileid="4" wangid="4,0,4,0,4,0,4,0"/>
+   <wangtile tileid="5" wangid="4,0,7,0,5,0,4,0"/>
+   <wangtile tileid="6" wangid="4,0,4,0,10,0,7,0"/>
+   <wangtile tileid="7" wangid="2,0,1,0,0,0,0,0"/>
+   <wangtile tileid="8" wangid="4,0,4,0,2,0,1,0"/>
+   <wangtile tileid="9" wangid="4,0,1,0,0,0,1,0"/>
+   <wangtile tileid="10" wangid="2,0,4,0,2,0,0,0"/>
+   <wangtile tileid="11" wangid="4,0,4,0,4,0,4,0"/>
+   <wangtile tileid="12" wangid="5,0,9,0,4,0,4,0"/>
+   <wangtile tileid="13" wangid="10,0,4,0,4,0,9,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
