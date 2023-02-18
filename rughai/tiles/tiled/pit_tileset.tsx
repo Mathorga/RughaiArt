7cc943532c92f21b8690568661e4e017905d0cb6
Simone Branchesi <simobranca1@gmail.com>
2023-02-18 13:32:25 +0100

    added rughai walls,pit,lower layer

--- rughai/tiles/tiled/pit_tileset.tsx
+++ rughai/tiles/tiled/pit_tileset.tsx
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="pit_depth" tilewidth="8" tileheight="16" tilecount="14" columns="7">
- <image source="../RughaiArt/rughai/tiles/pit_tileset.png" width="56" height="32"/>
+<tileset version="1.9" tiledversion="1.9.2" name="pit_tileset" tilewidth="8" tileheight="16" tilecount="16" columns="8">
+ <image source="../pit_tileset.png" width="64" height="32"/>
  <tile id="3" probability="80"/>
  <tile id="4" probability="10"/>
- <tile id="11" probability="10"/>
+ <tile id="12" probability="10"/>
  <wangsets>
-  <wangset name="Unnamed Set" type="edge" tile="-1">
+  <wangset name="fill" type="edge" tile="-1">
    <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
    <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
    <wangcolor name="" color="#0000ff" tile="-1" probability="1"/>
@@ -23,13 +23,46 @@
    <wangtile tileid="4" wangid="4,0,4,0,4,0,4,0"/>
    <wangtile tileid="5" wangid="4,0,7,0,5,0,4,0"/>
    <wangtile tileid="6" wangid="4,0,4,0,10,0,7,0"/>
-   <wangtile tileid="7" wangid="2,0,1,0,0,0,0,0"/>
-   <wangtile tileid="8" wangid="4,0,4,0,2,0,1,0"/>
-   <wangtile tileid="9" wangid="4,0,1,0,0,0,1,0"/>
-   <wangtile tileid="10" wangid="2,0,4,0,2,0,0,0"/>
-   <wangtile tileid="11" wangid="4,0,4,0,4,0,4,0"/>
-   <wangtile tileid="12" wangid="5,0,9,0,4,0,4,0"/>
-   <wangtile tileid="13" wangid="10,0,4,0,4,0,9,0"/>
+   <wangtile tileid="7" wangid="4,0,4,0,5,0,4,0"/>
+   <wangtile tileid="8" wangid="2,0,1,0,0,0,0,0"/>
+   <wangtile tileid="9" wangid="4,0,4,0,2,0,1,0"/>
+   <wangtile tileid="10" wangid="4,0,1,0,0,0,1,0"/>
+   <wangtile tileid="11" wangid="2,0,4,0,2,0,0,0"/>
+   <wangtile tileid="12" wangid="4,0,4,0,4,0,4,0"/>
+   <wangtile tileid="13" wangid="5,0,9,0,4,0,4,0"/>
+   <wangtile tileid="14" wangid="10,0,4,0,4,0,9,0"/>
+  </wangset>
+  <wangset name="borders" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="3,0,1,0,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,0,3,0,1,0"/>
+   <wangtile tileid="2" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="8" wangid="3,0,2,0,0,0,0,0"/>
+   <wangtile tileid="9" wangid="0,0,0,0,3,0,2,0"/>
+   <wangtile tileid="10" wangid="0,0,2,0,0,0,2,0"/>
+  </wangset>
+  <wangset name="boulder_1" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="" color="#ff7700" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00e9ff" tile="-1" probability="1"/>
+   <wangtile tileid="5" wangid="0,0,5,0,1,0,0,0"/>
+   <wangtile tileid="6" wangid="0,0,0,0,2,0,5,0"/>
+   <wangtile tileid="12" wangid="0,0,4,0,0,0,0,0"/>
+   <wangtile tileid="13" wangid="1,0,3,0,0,0,4,0"/>
+   <wangtile tileid="14" wangid="2,0,0,0,0,0,3,0"/>
+  </wangset>
+  <wangset name="boulder_2" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="7" wangid="0,0,0,0,3,0,0,0"/>
+   <wangtile tileid="12" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="13" wangid="3,0,2,0,0,0,1,0"/>
+   <wangtile tileid="14" wangid="0,0,0,0,0,0,2,0"/>
   </wangset>
  </wangsets>
 </tileset>
