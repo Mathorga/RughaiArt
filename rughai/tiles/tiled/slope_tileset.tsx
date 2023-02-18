e936b81a5307cdf1e531729b831ba884f5f4342f
Simone Branchesi <simobranca1@gmail.com>
2023-02-18 17:22:47 +0100

    added tiles

---
+++ rughai/tiles/tiled/slope_tileset.tsx
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="slope_tileset" tilewidth="8" tileheight="8" tilecount="45" columns="5">
+ <image source="../slope_tileset.png" width="40" height="72"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="edge" tile="-1">
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="vertical" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="horizontal" color="#ff7700" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00e9ff" tile="-1" probability="1"/>
+   <wangcolor name="" color="#ff00d8" tile="-1" probability="1"/>
+   <wangtile tileid="10" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="11" wangid="2,0,0,0,2,0,0,0"/>
+   <wangtile tileid="15" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="16" wangid="2,0,0,0,2,0,0,0"/>
+   <wangtile tileid="20" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="21" wangid="2,0,0,0,2,0,0,0"/>
+   <wangtile tileid="25" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="26" wangid="2,0,0,0,2,0,0,0"/>
+   <wangtile tileid="27" wangid="0,0,3,0,5,0,3,0"/>
+   <wangtile tileid="28" wangid="0,0,3,0,5,0,3,0"/>
+   <wangtile tileid="30" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="31" wangid="2,0,0,0,0,0,0,0"/>
+   <wangtile tileid="32" wangid="5,0,4,0,0,0,4,0"/>
+   <wangtile tileid="33" wangid="5,0,4,0,0,0,4,0"/>
+   <wangtile tileid="36" wangid="0,0,3,0,5,0,0,0"/>
+   <wangtile tileid="37" wangid="0,0,3,0,5,0,3,0"/>
+   <wangtile tileid="38" wangid="0,0,3,0,5,0,3,0"/>
+   <wangtile tileid="41" wangid="5,0,4,0,0,0,0,0"/>
+   <wangtile tileid="42" wangid="5,0,4,0,0,0,4,0"/>
+   <wangtile tileid="43" wangid="5,0,4,0,0,0,4,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
