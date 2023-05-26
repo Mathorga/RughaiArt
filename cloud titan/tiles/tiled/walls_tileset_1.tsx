9c8a7929bfc871fb2049be47d5531a43dd83cc57
Luka Micheletti <l.micheletti@ebw.it>
2023-05-27 00:27:49 +0200

    new walls and floor tilesets

--- cloud titan/tiles/tiled/walls_tileset_1.tsx
+++ cloud titan/tiles/tiled/walls_tileset_1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="walls_tileset_1" tilewidth="8" tileheight="8" tilecount="48" columns="6">
- <image source="../tilesets/walls_tileset_1.png" width="48" height="64"/>
+<tileset version="1.9" tiledversion="1.9.2" name="walls_tileset_1" tilewidth="8" tileheight="8" tilecount="48" columns="5">
+ <image source="../tilesets/walls_tileset_1.png" width="40" height="40"/>
 </tileset>
