afce0d20164882b50a7f533660479f8f7519dddf
Simone Branchesi <simobranca1@gmail.com>
2023-06-17 12:19:48 +0200

    resized rooms

--- cloud titan/tiles/tiled/walls_tileset_0.tsx
+++ cloud titan/tiles/tiled/walls_tileset_0.tsx
@@ -1,38 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="walls_tileset_0" tilewidth="8" tileheight="8" tilecount="48" columns="6">
- <image source="../tilesets/walls_tileset_0.png" width="48" height="64"/>
- <wangsets>
-  <wangset name="walls" type="edge" tile="-1">
-   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
-   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
-   <wangcolor name="" color="#0000ff" tile="-1" probability="1"/>
-   <wangcolor name="" color="#ff7700" tile="-1" probability="1"/>
-   <wangcolor name="" color="#00e9ff" tile="-1" probability="1"/>
-   <wangcolor name="" color="#ff00d8" tile="-1" probability="1"/>
-   <wangcolor name="" color="#ffff00" tile="-1" probability="1"/>
-   <wangtile tileid="0" wangid="0,0,2,0,1,0,0,0"/>
-   <wangtile tileid="1" wangid="0,0,3,0,1,0,2,0"/>
-   <wangtile tileid="2" wangid="0,0,2,0,1,0,3,0"/>
-   <wangtile tileid="3" wangid="0,0,0,0,1,0,2,0"/>
-   <wangtile tileid="6" wangid="1,0,0,0,0,0,0,0"/>
-   <wangtile tileid="12" wangid="0,0,2,0,1,0,0,0"/>
-   <wangtile tileid="13" wangid="0,0,3,0,1,0,2,0"/>
-   <wangtile tileid="14" wangid="0,0,2,0,1,0,3,0"/>
-   <wangtile tileid="15" wangid="0,0,0,0,1,0,2,0"/>
-   <wangtile tileid="20" wangid="1,0,0,0,0,0,0,0"/>
-   <wangtile tileid="24" wangid="4,0,0,0,6,0,0,0"/>
-   <wangtile tileid="25" wangid="4,0,0,0,7,0,0,0"/>
-   <wangtile tileid="26" wangid="0,0,2,0,4,0,0,0"/>
-   <wangtile tileid="27" wangid="0,0,0,0,4,0,2,0"/>
-   <wangtile tileid="28" wangid="4,0,0,0,5,0,0,0"/>
-   <wangtile tileid="29" wangid="4,0,0,0,5,0,0,0"/>
-   <wangtile tileid="30" wangid="6,0,0,0,0,0,0,0"/>
-   <wangtile tileid="31" wangid="7,0,0,0,0,0,0,0"/>
-   <wangtile tileid="34" wangid="5,0,0,0,0,0,0,0"/>
-   <wangtile tileid="36" wangid="0,0,0,0,4,0,0,0"/>
-   <wangtile tileid="37" wangid="0,0,0,0,4,0,0,0"/>
-   <wangtile tileid="38" wangid="4,0,0,0,4,0,0,0"/>
-   <wangtile tileid="39" wangid="4,0,0,0,4,0,0,0"/>
-  </wangset>
- </wangsets>
+<tileset version="1.9" tiledversion="1.9.2" name="walls_tileset_0" tilewidth="8" tileheight="8" tilecount="25" columns="5">
+ <image source="../tilesets/walls_tileset_0.png" width="40" height="40"/>
 </tileset>
